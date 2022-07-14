b3ca3bf7306b6b4476ed1b3e72c50d361e970480
Дарья Опякина <daryaopyakina@mail.ru>
2022-07-14 16:32:10 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance;
+        balance = 100;
+        int donation1;
+        donation1 = 100;
+        int donation2;
+        donation2 = 1100;
+        System.out.println(balance + donation1);
+        {
+                System.out.println(balance + donation2 + 11);
+        }
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
+}
