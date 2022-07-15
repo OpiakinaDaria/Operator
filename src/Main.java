9d66eca07a2bfda5299a4a8eb815ad00eb7b5e54
Дарья Опякина <daryaopyakina@mail.ru>
2022-07-15 17:20:38 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         int balance;
-        balance = 100;
+        balance = 250;
         int donation1;
-        donation1 = 100;
+        donation1 = 500;
         int donation2;
-        donation2 = 1100;
-        System.out.println(balance + donation1);
-        {
-                System.out.println(balance + donation2 + 11);
+        donation2 = 1500;
+        int bonus1;
+        bonus1 = donation1 / 100;
+        int bonus2;
+        bonus2 = donation2 / 100;
+        if (donation1 < 1000) { System.out.println (donation1 + balance);
+        }    else  { System.out.println (donation1 + balance + bonus1);
         }
-
+        if (donation2 < 1000) {
+            System.out.println(donation2 + balance);
+        }   else  { System.out.println (donation2 + balance + bonus2);
+        }
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
